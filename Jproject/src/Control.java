82dab1f3419ea7b2c4fbe3feff305af606664794
aa <aa@DESKTOP-OCIGRN5>
2017-10-18 18:55:34 +0900

    Control >> 주문자, 결제자 랜덤으로 정하는 메소드 구현 DAO >> 주문자, 결제자 결정해서 member테이블의 상태를 업데이트 메소드 구현

--- Jproject/src/Control.java
+++ Jproject/src/Control.java
@@ -1,4 +1,29 @@
+import java.util.Random;
 
 public class Control {
+	private DAO dao = new DAO();
+	private Random ran = null;
+	private int num1 = 0, num2 = 0; // 랜덤수 저장
+
+	public void randomSelect(String[] src) {
+		/*
+		 * String 배열 src(당일 하나의 업체에 주문한 사람들의 이름 배열) 
+		 * 랜덤으로 한 사람을 뽑아 주문자로 업데이트 
+		 * 랜덤으로 한 사람을 뽑아 결제자로 업데이트
+		 */
+
+		if (src.length >= 2) {
+
+			while (num1 == num2) { // 중복되지 않게 랜덤수 2개 반환
+				num1 = ran.nextInt(src.length);
+				num2 = ran.nextInt(src.length);
+			}
+
+			dao.update(src[num1], 2); // num1 >> 주문자
+			dao.update(src[num2], 3); // num2 >> 결제자
+
+		}
+
+	}
 
 }
--- Jproject/src/DAO.java
+++ Jproject/src/DAO.java
@@ -1,4 +1,62 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class DAO {
+	
+	private Connection con = null;
+	private PreparedStatement psmt = null;
+	private ResultSet rs = null;
+	
+	private String url = null;
+	private String user = null;
+	private String pw = null;
+
+	// DB 연동
+	private void getConnection() throws ClassNotFoundException, SQLException { 
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		con = DriverManager.getConnection(url, user, pw);
+	}
+	
+	// 생성자 기입 사항 
+	// >> url, user, pw
+	public DAO(String url, String user, String pw) {
+		this.url = url;
+		this.user = user;
+		this.pw = pw;
+	}
+	
+	
+	public void update(String string, int num) {
+		// 주문자, 결제자 결정해서 member테이블의 상태를 업데이트
+		
+		try {
+			getConnection();
+			
+			String sql = "update member set userstate = ? where name = ?";
+			psmt = con.prepareStatement(sql);
+			psmt.setString(2, string);
+			psmt.setInt(1, num);
+
+			result = psmt.executeUpdate();
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally { 
+			try {
+				
+				if(psmt!=null)psmt.close();
+				if(con!=null)con.close();
+				
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		
+		
+	}
 
 }
